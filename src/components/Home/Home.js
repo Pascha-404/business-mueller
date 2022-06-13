868c83a8feeccaa9ec15e723c88eb0508d341207
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-13 10:58:04 +0200

    add Home component to display home-view

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import HeroSection from '../HeroSection';
+
+// Gathers all Sections to display Homeview in main section
+function Home() {
+    return <Fragment>
+        <HeroSection />
+    </Fragment>;
+}
+
+export default Home;
