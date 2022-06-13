858b719ffe4ad8b6c5ed7bb226122ffaa44b664e
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-13 09:53:34 +0200

    import sass main file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import './sass/main.scss'
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
