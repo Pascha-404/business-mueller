aca26e31b8eb74e0ea7dc7e0f100d1d2e438450c
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-13 10:10:59 +0200

    add first structure/components, delete unused style file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,16 @@
-import logo from '../../assets/logo.svg';
-import './App.style.scss';
+import { Fragment } from 'react';
+
+import Navbar from '../Navbar/Navbar';
+import HeroSection from '../HeroSection';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Fragment>
+			<header><Navbar /></header>
+			<main>
+				<HeroSection />
+			</main>
+		</Fragment>
 	);
 }
 
