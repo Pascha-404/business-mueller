bbb7dfc4161c98adf727fcb16c71b5095298cb30
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-15 16:53:56 +0200

    add starter code, import AboutSection in Home component

---
+++ src/components/AboutSection/AboutSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './AboutSection.module.scss';
+
+function AboutSection() {
+	return <div>AboutSection</div>;
+}
+
+export default AboutSection;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 import HeroSection from '../HeroSection';
+import AboutSection from '../AboutSection';
 
-// Gathers all Sections to display Homeview in main section
+// Gathers all Sections to display Homeview in <main> element
 function Home() {
-    return <Fragment>
-        <HeroSection />
-    </Fragment>;
+	return (
+		<Fragment>
+			<HeroSection />
+			<AboutSection />
+		</Fragment>
+	);
 }
 
 export default Home;
