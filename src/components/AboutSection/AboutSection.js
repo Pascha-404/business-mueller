2e51c9417b0fa9cb88dee5dd711f92b0dd946ba0
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-15 17:21:30 +0200

    add img and text content for first row

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import aboutImg from '../../assets/img-about.png';
+import textureImg from '../../assets/texture-2.png';
 
 import styles from './AboutSection.module.scss';
 
 function AboutSection() {
-	return <div>AboutSection</div>;
+	return (
+		<section className={styles.aboutSection}>
+			<div className={styles.aboutFirstRow}>
+				<div className={styles.imgWrapper}>
+					<img src={aboutImg} alt='Waschtisch und Fliesen in Marmor' />
+					<img
+						src={textureImg}
+						alt='Weiße Steintextur'
+						className={styles.underlayedImg}
+					/>
+				</div>
+				<div>
+					<h2>
+						Planlos geht der <span className={styles.highlight}>Plan los!</span>
+					</h2>
+					<p>
+						Wir spezalisieren uns auf Trockenbau und Fliesen nicht nur in Ihrem Bad als
+						auch Küchen, Pools und weiteren Räumlichkeiten.
+					</p>
+					<p>
+						Um Ihre induviduellen Gestaltungswünsche wird sich in unseren Unternehmen
+						gerne gekümmert.
+					</p>
+					<p>
+						Unser hilfbereites und freundliches Team steht Ihnen gernen für Fachberatungen
+						und Rüchsprachen zur Verfügung. Schreiben oder Rufen Sie uns an. Wir freuen
+						uns auf Ihre Projektideen.
+					</p>
+				</div>
+			</div>
+			<div></div>
+		</section>
+	);
 }
 
 export default AboutSection;
