be73d4da1d1c5b7a8419edd30d00ad5d492162c7
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-16 18:09:45 +0200

    delete react logo, add index file

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import iconCompass from './icon-compass.svg';
+import iconPhone from './icon-phone.png';
+import imgAbout from './img-about.png';
+import imgHero from './img-hero.png';
+import inspoOne from './inspo-1.png';
+import inspoThree from './inspo-3.png';
+import logoMueller from './logo-mueller.png';
+import bgService from './service-bg.png';
+import textureOne from './texture-1.png';
+import textureTwo from './texture-2.png';
+
+export {
+	iconCompass,
+	iconPhone,
+	imgAbout,
+	imgHero,
+	inspoOne,
+	inspoThree,
+	logoMueller,
+	bgService,
+	textureOne,
+	textureTwo,
+};
