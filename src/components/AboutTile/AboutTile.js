7197f1a20ec5a59c81489a343d39e6ffa2b8a7bf
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-29 11:30:22 +0200

    add classes and comment

--- src/components/AboutTile/AboutTile.js
+++ src/components/AboutTile/AboutTile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './AboutTile.module.scss';
 
+// Simple component to display cards/tiles for about section
 function AboutTile({ icon, iconAlt, title, text }) {
 	return (
 		<div className={styles.aboutTile}>
@@ -8,8 +9,8 @@ function AboutTile({ icon, iconAlt, title, text }) {
 				<div className={styles.imgBlur} />
 				<img src={icon} alt={iconAlt} />
 			</div>
-			<h3>{title}</h3>
-			<p>{text}</p>
+			<h3 className={styles.title}>{title}</h3>
+			<p className={styles.pTiles}>{text}</p>
 		</div>
 	);
 }
