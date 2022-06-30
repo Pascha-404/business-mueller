a397539c5f3ce7f22b13f9c9d4e9f561828454fe
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-30 12:35:59 +0200

    add Text to section

--- src/components/InspoSection/InspoSection.js
+++ src/components/InspoSection/InspoSection.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-import styles from './InspoSection.module.scss'
+import styles from './InspoSection.module.scss';
 
 function InspoSection() {
-  return (
-    <section className={styles.inspoSection}>InspoSection</section>
-  )
+	return (
+		<section className={styles.inspoSection}>
+			<h2>
+				Unsere <span className={styles.highlight}>Inspirationen</span> für Sie
+			</h2>
+			<p>
+				Wir sind bereit für jedes Projekt, und wenn Sie wissen nicht, was zu bauen, wir
+				...
+			</p>
+		</section>
+	);
 }
 
-export default InspoSection
+export default InspoSection;
