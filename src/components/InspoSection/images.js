5ef03a0c3eb35128ad5997d4c7c8ed9ce6bd7a99
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-07-01 12:45:54 +0200

    add images file for images in carousel

---
+++ src/components/InspoSection/images.js
@@ -0,0 +1,19 @@
+const images = [
+	{
+		id: 1,
+		src: '../../assets/inspo-1.png',
+		alt: 'Waschtisch mit Holzakzenten',
+	},
+	{
+		id: 2,
+		src: '../../assets/img-about.png',
+		alt: 'Waschtisch und Fliesen in Marmor',
+	},
+	{
+		id: 3,
+		src: '../../assets/inspo-3.png',
+		alt: 'Bad mit Fliesen in Holzoptik',
+	},
+];
+
+export default images;
