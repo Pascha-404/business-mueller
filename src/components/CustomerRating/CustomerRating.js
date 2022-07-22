43360195d9e9156e06e748a2da9c46dface52d50
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-07-22 18:46:58 +0200

    add content and styling to component

--- src/components/CustomerRating/CustomerRating.js
+++ src/components/CustomerRating/CustomerRating.js
@@ -5,9 +5,11 @@ import styles from './CustomerRating.module.scss';
 function CustomerRating({ review, customerName, customerInfo }) {
 	return (
 		<article className={styles.customerRating}>
-			<p>{review}</p>
-			<p>{customerName}</p>
-			<p>{customerInfo}</p>
+			<p className={styles.review}>{review}</p>
+			<div className={styles.customerDetails}>
+                <p className={styles.customerName}>{customerName}</p>
+                <p className={styles.customerInfo}>{customerInfo}</p>
+            </div>
 		</article>
 	);
 }
