acf9f37b3b629f2b4c06d22025b583d81c63dc8b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-08-23 19:14:01 +0200

    add starter files for context + contact icon

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import HeroSection from '../HeroSection';
 import AboutSection from '../AboutSection';
 import InspoSection from '../InspoSection';
