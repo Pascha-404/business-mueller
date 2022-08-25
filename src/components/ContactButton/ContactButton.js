9da76e09b7a7a2a8f7743a59438ad9dafb268699
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-08-25 20:26:20 +0200

    add first styling and structure to ContactButton

--- src/components/ContactButton/ContactButton.js
+++ src/components/ContactButton/ContactButton.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import styles from './ContactButton.module.scss';
 
+import { useContactDispatch } from '../../contexts/contact.context';
+
+import { iconContact } from '../../assets';
+
 function ContactButton() {
-	return <div>ContactButton</div>;
+	const contactDispatch = useContactDispatch();
+	return (
+		<button
+			className={styles.contactButton}
+			onClick={() => {
+				contactDispatch({ type: 'TOGGLE_FORM' });
+			}}>
+			<img src={iconContact} alt='Contact Icon' />
+		</button>
+	);
 }
 
 export default ContactButton;
