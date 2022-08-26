a6db4ec638afdaf8995f57e80ab8436ea0cb497f
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-08-26 19:21:49 +0200

    add ContactForm to home component with first structure and styling

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import styles from './ContactForm.modules.scss';
+import styles from './ContactForm.module.scss';
 
 function ContactForm() {
-	return <div>ContactForm</div>;
+	return (
+		<section className={styles.formWrapper}>
+			<div className={styles.contactForm}>
+				<p>
+					Wir freuen uns auf Ihre Nachricht und werden uns schnellst möglich bei Ihnen
+					zurückmelden.
+				</p>
+				<form>
+					<label htmlFor='name'>Name</label>
+					<input type='text' id='name' />
+					<label htmlFor='email'>E-Mail Adresse</label>
+					<input type='text' id='email' />
+					<label htmlFor='message'>Nachricht</label>
+					<textarea name='message' id='message' cols='30' rows='10'></textarea>
+					<label htmlFor='phoneCall'>Wünschen Sie einen Rückruf?</label>
+					<input type='checkbox' name='phoneCall' id='phoneCall' />
+				</form>
+			</div>
+		</section>
+	);
 }
 
 export default ContactForm;
