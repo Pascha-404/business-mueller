bb640b221bd13391d1e759f3c270fefedd7b6a74
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-08-28 19:33:24 +0200

    add cases for toggle submit/phoneCall and handleChange of key values

--- src/reducers/contact.reducer.js
+++ src/reducers/contact.reducer.js
@@ -2,6 +2,12 @@ const reducer = (state, action) => {
 	switch (action.type) {
 		case 'TOGGLE_FORM':
 			return { ...state, formIsActive: !state.formIsActive };
+		case 'TOGGLE_PHONECALL':
+			return { ...state, phoneCall: !state.phoneCall };
+		case 'TOGGLE_SUBMIT':
+			return { ...state, submit: !state.submit };
+		case 'HANDLE_CHANGE':
+			return { ...state, [action.key]: action.value };
 		default:
 			return state;
 	}
