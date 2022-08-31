52caba0b9c37c0304214e3adc2a9392bcb1c1aa4
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-08-31 14:21:49 +0200

    add function that triggers email on backend

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,43 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
+	auth: {
+		user: process.env.REACT_APP_MAIL_USER,
+		pass: process.env.REACT_APP_MAIL_PASSWORD,
+	},
+});
+
+exports.sendEmail = functions.firestore
+	.document('mails/{mailId}')
+	.onCreate((snap, context) => {
+		const mailOptions = {
+			from: process.env.REACT_APP_MAIL_USER,
+			to: process.env.REACT_APP_MAIL_RECIEVER,
+			replyTo: snap.data().mail,
+			subject: `Anfrage von ${snap.data().name}`,
+			html: `<h1>Kontaktformular eingegangen</h1>
+                    <br>
+                    <p>Name: ${snap.data().name}</p>
+                    <p>Email: ${snap.data().mail}</p>
+                    <p>Telefonnummer: ${snap.data().phoneNumber}</p>
+                    <p>Nachricht: ${snap.data().message}</p>`,
+		};
+
+		return transporter.sendMail(mailOptions, (error, data) => {
+			if (error) {
+				console.log(error);
+				return;
+			}
+			console.log('Mail Sent!');
+		});
+	});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
