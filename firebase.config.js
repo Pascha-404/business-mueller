5c34c28e276139c456449258b78b00cd20bc5565
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-08-31 14:50:50 +0200

    add and initialize firestore

---
+++ firebase.config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+	authDomain: `${process.env.REACT_APP_FIREBASE_DOMAIN}.firebaseapp.com`,
+	projectId: process.env.REACT_APP_FIREBASE_DOMAIN,
+	storageBucket: `${process.env.REACT_APP_FIREBASE_DOMAIN}.appspot.com`,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDERID,
+	appId: process.env.REACT_APP_FIREBASE_APPID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
