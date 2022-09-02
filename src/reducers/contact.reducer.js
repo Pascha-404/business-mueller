9798ba67b8a39512104e7147e46dad50235dc2ec
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-09-02 16:26:15 +0200

    add case to reset the state

--- src/reducers/contact.reducer.js
+++ src/reducers/contact.reducer.js
@@ -8,6 +8,16 @@ const reducer = (state, action) => {
 			return { ...state, submit: !state.submit };
 		case 'HANDLE_CHANGE':
 			return { ...state, [action.key]: action.value };
+		case 'RESET_STATE':
+			return {
+				formIsActive: false,
+				name: '',
+				email: '',
+				message: '',
+				phoneCall: false,
+				phoneNumber: '',
+				submit: false,
+			};
 		default:
 			return state;
 	}
