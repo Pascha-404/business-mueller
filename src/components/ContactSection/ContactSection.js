73462895f13116c7ca23baf569131d7acdb94c99
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-09-25 12:38:56 +0200

    add text, images and styling

--- src/components/ContactSection/ContactSection.js
+++ src/components/ContactSection/ContactSection.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ContactForm from '../ContactForm/ContactForm';
+
+import { textureTwo, imgContact } from '../../assets';
+
 import styles from './ContactSection.module.scss';
 
 function ContactSection() {
 	return (
 		<section className={styles.contactSection}>
-			<ContactForm />
+			<div className={styles.imgWrapper}>
+                <img src={textureTwo} alt='Weiße Steintextur' className={styles.underlayedImg} />
+                <img src={imgContact} alt="Weißes Badezimmer mit schwarz-goldenen Akzenten" />
+			</div>
+			<div className={styles.contactFormWrapper}>
+				<h2>
+					Schreiben Sie uns um zusammen Ihr
+					<span className={styles.highlight}> neues Projekt</span> zu planen
+				</h2>
+				<p>
+					Bitte hinterlassen Sie uns ihre bevorzugten Kontaktdaten - Email oder Telefon,
+					damit wir uns mit Ihnen in Verbindung setzen und Ihr Projekt besprechen können.
+				</p>
+				<ContactForm />
+			</div>
 		</section>
 	);
 }
