ee989061803985546a6544027a2f552460a8d8a7
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-09-27 00:52:39 +0200

    add spacing, wrapper for checkbox and show phoneNumber input if checkbox is active

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -24,36 +24,60 @@ function ContactForm() {
 
 	return (
 		<form onSubmit={handleSubmit} className={styles.contactForm}>
-			<input type='text' id='name' value={name} onChange={handleChange('name')} />
+			<input
+				type='text'
+				id='name'
+				value={name}
+				onChange={handleChange('name')}
+				required
+			/>
 			<label htmlFor='name'>Vorname und Name</label>
-			<input type='email' id='email' value={email} onChange={handleChange('email')} />
+
+			<input
+				type='email'
+				id='email'
+				value={email}
+				onChange={handleChange('email')}
+				required
+			/>
 			<label htmlFor='email'>E-Mail Adresse</label>
+
 			<textarea
 				name='message'
 				id='message'
 				cols='30'
 				rows='10'
 				value={message}
-				onChange={handleChange('message')}></textarea>
+				onChange={handleChange('message')}
+				required></textarea>
 			<label htmlFor='message'>Nachricht</label>
-			<input
-				type='checkbox'
-				name='phoneCall'
-				id='phoneCall'
-				value={phoneCall}
-				onChange={togglePhonecall}
-			/>
-			<label htmlFor='phoneCall'>Wünschen Sie einen Rückruf?</label>
-			<input
-				type='tel'
-				name='phoneNumber'
-				id='phoneNumber'
-				value={phoneNumber}
-				onChange={handleChange('phoneNumber')}
-			/>
-			<label htmlFor='phoneNumber'>Telefonnummer</label>
+
+			<div className={styles.checkboxWrapper}>
+				<input
+					type='checkbox'
+					name='phoneCall'
+					id='phoneCall'
+					value={phoneCall}
+					onChange={togglePhonecall}
+				/>
+				<label htmlFor='phoneCall'>Wünschen Sie einen Rückruf?</label>
+			</div>
+
+			{phoneCall && (
+				<React.Fragment>
+					<input
+						type='tel'
+						name='phoneNumber'
+						id='phoneNumber'
+						value={phoneNumber}
+						onChange={handleChange('phoneNumber')}
+					/>
+					<label htmlFor='phoneNumber'>Telefonnummer</label>
+				</React.Fragment>
+			)}
+
 			<div className={styles.btnWrapper}>
-				<Button type='button' text="Los geht's!" submit />
+				<Button type='button' text="Los geht's!" submit theme='dark' bigBtn />
 			</div>
 		</form>
 	);
