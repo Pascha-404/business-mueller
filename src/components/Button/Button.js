b8cb169bb8f7508ce89ec5c57d1aeefd55f1c0cd
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-09-27 17:07:16 +0200

    add code for light, dark und big btns, add propertys to change theme and size, add classes to stylefix centering of imgWrapper

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,17 +5,30 @@ import styles from './Button.module.scss';
 // Renders a Button or Button shaped element.
 // accepts type = button (default) | shaped.
 // buttons and button shaped components have different styling (dark and light)
-function Button({ type = 'button', text, submit = false }) {
+function Button({
+	type = 'button',
+	text,
+	submit = false,
+	theme = 'light',
+	bigBtn = false,
+}) {
+	// Constants define the passed style classes for theme and size of real button elements.
+	const btnTheme =
+		theme === 'light' ? styles.light : theme === 'dark' ? styles.dark : null;
+	const bigButton = bigBtn === true ? styles.bigBtn : '';
+
 	if (type === 'button' && submit === false) {
-		return <button className={styles.button}>{text}</button>;
+		return (
+			<button className={`${styles.button} ${btnTheme} ${bigButton}`}>{text}</button>
+		);
 	} else if (type === 'button' && submit === true) {
 		return (
-			<button className={styles.button} type='submit'>
+			<button className={`${styles.button} ${btnTheme} ${bigButton}`} type='submit'>
 				{text}
 			</button>
 		);
 	} else if (type === 'shaped') {
-		return <div className={styles.buttonShaped}>{text}</div>;
+		return <div className={`${styles.buttonShaped}`}>{text}</div>;
 	} else {
 		throw new Error("Type does not exist. Choose between 'button' or 'shaped'");
 	}
