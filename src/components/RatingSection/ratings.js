280e2c8dbddccd3c9e887f2227c8d87f67e5243b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-10-08 17:22:30 +0200

    add to each rating a titel and rating(number)

--- src/components/RatingSection/ratings.js
+++ src/components/RatingSection/ratings.js
@@ -1,17 +1,23 @@
 const ratings = [
 	{
+		titel: 'Tolle Arbeit!',
+		rating: 5,
 		review:
 			'Wir haben unser neues EFH von dem Fliesenfachbetrieb J. Müller Fliesen lassen und sind von Anfang bis zum Ende super beraten worden! Zwei Bäder und Flur mit großflächigem Feinsteinzeug (60*90 cm und 1,20*1,20) alles ist perfekt gefliest! Wir sind begeistert und bedanken uns für die tollen Arbeiten. Zuverlässig, ordentlich und immer freundlich.',
 		customerName: 'Alfred Pinkel',
 		customerInfo: 'Ingeneur aus Berlin Tempelhof',
 	},
 	{
+		titel: 'Zuverlässig und sehr freundlich',
+		rating: 5,
 		review:
 			'Liebes Team Müller, vielen lieben Dank für die tolle Abwicklung. Die Fliesen in 1,2×1,2 m sind echt der Hammer. Sowohl die Sauberkeit wie auch die Verlegung und das Team hatten wir so noch nie erlebt! Es war einfach nur perfekt! Vielen Dank nochmal dafür.',
 		customerName: 'Alfred Pinkel',
 		customerInfo: 'Ingeneur aus Berlin Tempelhof',
 	},
 	{
+		titel: 'Bis nächstes mal',
+		rating: 5,
 		review:
 			'Sehr geehrter Herr Müller, vielen lieben Dank! Unser neues Bad ist vom Umbau alles aus einer Hand sagten Sie uns von Anfang bis zum Ende perfekt! Besonders gefallen hat uns das sie immer einen Vorschlag parat hatten Dankeschön.',
 		customerName: 'Alfred Pinkel',
