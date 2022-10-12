78904b39cfee5ff28c4f2b7c862338e305ea0b34
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-10-12 20:15:08 +0200

    add content cases for error and success of form submit

--- src/components/ContactSection/ContactSection.js
+++ src/components/ContactSection/ContactSection.js
@@ -1,28 +1,57 @@
 import React from 'react';
 import ContactForm from '../ContactForm/ContactForm';
 
+import { useContact } from '../../contexts/contact.context';
 import { textureTwo, imgContact } from '../../assets';
+
+import { iconError, iconSuccess } from '../../assets';
 
 import styles from './ContactSection.module.scss';
 
 function ContactSection() {
+	const { formState } = useContact();
 	return (
 		<section className={styles.contactSection} id='contactSection'>
 			<div className={styles.imgWrapper}>
-                <img src={textureTwo} alt='Weiße Steintextur' className={styles.underlayedImg} />
-                <img src={imgContact} alt="Weißes Badezimmer mit schwarz-goldenen Akzenten" />
+				<img src={textureTwo} alt='Weiße Steintextur' className={styles.underlayedImg} />
+				<img src={imgContact} alt='Weißes Badezimmer mit schwarz-goldenen Akzenten' />
 			</div>
-			<div className={styles.contactFormWrapper}>
-				<h2>
-					Schreiben Sie uns um zusammen Ihr
-					<span className={styles.highlight}> neues Projekt</span> zu planen
-				</h2>
-				<p>
-					Bitte hinterlassen Sie uns ihre bevorzugten Kontaktdaten - Email oder Telefon,
-					damit wir uns mit Ihnen in Verbindung setzen und Ihr Projekt besprechen können.
-				</p>
-				<ContactForm />
-			</div>
+			{formState === 'form' && (
+				<div className={styles.contactFormWrapper}>
+					<h2>
+						Schreiben Sie uns um zusammen Ihr
+						<span className={styles.highlight}> neues Projekt</span> zu planen
+					</h2>
+					<p>
+						Bitte hinterlassen Sie uns ihre bevorzugten Kontaktdaten - Email oder Telefon,
+						damit wir uns mit Ihnen in Verbindung setzen und Ihr Projekt besprechen
+						können.
+					</p>
+					<ContactForm />
+				</div>
+			)}
+			{formState === 'success' && (
+				<div className={styles.feedback}>
+					<img src={iconSuccess} alt='Haken Symbol' />
+					<p className={styles.h1}>Fertig!</p>
+					<p>
+						Ihre Nachricht wurde erfolgreich abgeschickt. Sie erhalten eine Kopie an die
+						angegebene Email Adresse. Dankeschön, wir melden uns schnellst möglich bei
+						Ihnen.
+					</p>
+				</div>
+			)}
+			{formState === 'error' && (
+				<div className={styles.feedback}>
+					<img src={iconError} alt='Kreuz Symbol' />
+					<p className={styles.h1}>Fertig!</p>
+					<p>
+						Ihre Nachricht wurde erfolgreich abgeschickt. Sie erhalten eine Kopie an die
+						angegebene Email Adresse. Dankeschön, wir melden uns schnellst möglich bei
+						Ihnen.
+					</p>
+				</div>
+			)}
 		</section>
 	);
 }
