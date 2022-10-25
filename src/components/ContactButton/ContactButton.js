f57cf6c77e0f94da14cbbe12a53f86ca0441a231
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-10-25 20:09:13 +0200

    add dynamic class for btnTheme, use BtnThemeContext for state

--- src/components/ContactButton/ContactButton.js
+++ src/components/ContactButton/ContactButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './ContactButton.module.scss';
 
-
 import { iconContact } from '../../assets';
+import { useBtnTheme } from '../../contexts/btnTheme.context';
 
 function ContactButton() {
-
+	const { isLight } = useBtnTheme();
 	return (
-		<div className={styles.contactButton}>
+		<div className={`${styles.contactButton} ${isLight ? styles.lightTheme : ''}`} id='contactBtn'>
 			<a href='#contactSection'>
 				<img src={iconContact} alt='Contact Icon' />
 			</a>
