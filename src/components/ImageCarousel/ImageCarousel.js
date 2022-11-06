2b40748dfc9ebaaa9a6b727e061b27539e6ce254
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-11-06 17:30:53 +0100

    get window innerWidth to change slidesPerPreview and spaceBetween till breakpoint size m(tablet)

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -9,14 +9,16 @@ import 'swiper/scss/pagination';
 import './ImageCarousel.scss';
 
 function ImageCarousel({ images }) {
+	const windowWidth = window.innerWidth;
+
 	return (
 		<Swiper
 			modules={[Navigation, A11y, Pagination]}
-			spaceBetween={-20}
-			slidesPerView={3}
+			spaceBetween={windowWidth < 870 ? -50 : windowWidth < 1200 ? -150 : -20}
+			slidesPerView={windowWidth < 1200 ? 2 : 3}
 			navigation
 			loop
-			pagination={{clickable: true}}
+			pagination={{ clickable: true }}
 			centeredSlides
 			speed={300}
 			className='imageCarousel'>
