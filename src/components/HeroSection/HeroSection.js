e779ebf98c52c929b051878e9ff2f082361eeeda
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-03-24 21:42:30 +0100

    add whitespace for better readability of code

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -21,15 +21,19 @@ function HeroSection() {
 						className={styles.underlayedImg}
 					/>
 				</div>
+
 				<div className={styles.btnWrap}>
 					<Button type='shaped' text='Badezimmer' />
 					<Button type='shaped' text='Küche' />
 					<Button type='shaped' text='Pool' />
 				</div>
+
 				<h1>
 					Einfach eine völlig <span className={styles.highlight}>neue Erfahrung</span>
 				</h1>
+
 				<h2 className={styles.h2}>Fliesen und andere Reparaturarbeiten</h2>
+
 				<div className={styles.hideOnSmall}>
 					<Button text='Mehr über uns' />
 					<h2 className={styles.heroLocation}>
@@ -37,27 +41,32 @@ function HeroSection() {
 					</h2>
 				</div>
 			</div>
+
 			<div className={styles.heroRight}>
 				<div className={`${styles.imgWrapper} ${styles.hideOnSmall}`}>
 					<img src={heroImg} alt='Waschtisch mit goldener Armatur' />
+
 					<img
 						src={textureImg}
 						alt='Weiße Steintextur'
 						className={styles.underlayedImg}
 					/>
 				</div>
+
 				<div className={styles.heroDetails}>
 					<div className={styles.detailsItem}>
 						<div className={styles.logoContainer}>
 							<img src={compassLogo} alt='Compass' />
 						</div>
-						<p>Egal woher Sie aus Berlin oder Brandenburg kommen</p>
+						<p className={styles.heroDetailsP}>Egal woher Sie aus Berlin oder Brandenburg kommen</p>
 					</div>
+
 					<div className={styles.detailsItem}>
 						<div className={styles.logoContainer}>
 							<img src={phoneLogo} alt='Phone' />
 						</div>
-						<p>
+
+						<p className={styles.heroDetailsP}>
 							Rufen Sie uns an:{' '}
 							<a href='tel:+493051637522' className={styles.phoneNumber}>
 								030 5163 7522
