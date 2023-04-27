18a2e2942572c57795961ee6f2722cd3b24c8bff
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-04-27 23:38:19 +0200

    add useEffect to scroll sections into view if provided through route

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import HeroSection from '../HeroSection';
 import AboutSection from '../AboutSection';
 import InspoSection from '../InspoSection';
@@ -10,9 +10,22 @@ import CouponSection from '../CouponSection';
 import { useBtnTheme } from '../../contexts/btnTheme.context';
 
 // Gathers all Sections to display Homeview in <main> element
-function Home() {
+function Home({scrollTo}) {
 	const { isLight } = useBtnTheme();
 	
+	useEffect(() => {
+		let targetSection
+		if (scrollTo) {
+			targetSection = document.querySelector(`#${scrollTo}`)
+			targetSection.scrollIntoView();
+	  }
+	
+	  return () => {
+		targetSection = null
+	  }
+	}, [scrollTo])
+	
+
 	return (
 		<Fragment>
 			<ContactButton isLight={isLight} />
