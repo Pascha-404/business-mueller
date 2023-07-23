9bc8dccab7ba1c6f5920678764e233d865c2424f
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-07-23 16:12:56 +0200

    add Link to privacy policy page

--- src/components/CookieBanner/CookieBanner.js
+++ src/components/CookieBanner/CookieBanner.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import CookieConsent from 'react-cookie-consent';
 import { useCookies } from 'react-cookie';
 
@@ -72,7 +73,7 @@ function CookieBanner() {
 				</p>
 				<p>
 					Weitere Informationen zu den von uns verwendeten Cookies und deren Zweck finden
-					Sie in unserer Datenschutzerklärung.
+					Sie in unserer <Link style={{borderBottom: '2px solid white'}} to='/datenschutz'>Datenschutzerklärung</Link>.
 				</p>
 			</CookieConsent>
 		);
