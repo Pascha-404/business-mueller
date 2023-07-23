1aedee48e9ecfb33864064948880ecdd0da44c10
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-07-23 17:33:50 +0200

    add Route for Impressum Page/Component

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import App from './components/App';
 import Home from './pages/Home';
+import Impressum from './pages/Impressum';
 
 // Routing displays by default Home component
 function Routing() {
@@ -10,10 +11,11 @@ function Routing() {
 			<Route path='/' element={<App />}>
 				<Route index element={<Home />} />
 				<Route path='about' element={<Home scrollTo={'aboutSection'} />} />
-				<Route path='inspirationen' element={<Home scrollTo={'inspoSection'}/>} />
+				<Route path='inspirationen' element={<Home scrollTo={'inspoSection'} />} />
 				<Route path='neukunde' element={<Home scrollTo={'couponSection'} />} />
 				<Route path='bewertungen' element={<Home scrollTo={'ratingSection'} />} />
 				<Route path='kontakt' element={<Home scrollTo={'contactSection'} />} />
+				<Route path='impressum' element={<Impressum />} />
 			</Route>
 		</Routes>
 	);
