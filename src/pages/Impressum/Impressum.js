6ad8eb88d528d21a0483eafb865be568bafe3430
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-07-23 17:25:22 +0200

    add starter files and starter code

---
+++ src/pages/Impressum/Impressum.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function Impressum() {
+	return (
+		<section>
+			<h1>Impressum</h1>
+
+			<h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+			<p>
+				J&ouml;rg M&uuml;ller
+				<br />
+				Fliesenleger M&uuml;ller
+				<br />
+				Alt-Wittenau 67a
+				<br />
+				13437 Berlin
+			</p>
+
+			<h2>Kontakt</h2>
+			<p>
+				Telefon: 030 5163 7522
+				<br />
+				E-Mail: mail@fliesenleger-mueller.de
+			</p>
+
+			<h2>Umsatzsteuer-ID</h2>
+			<p>
+				Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a
+				Umsatzsteuergesetz:
+				<br />
+				asdadf
+			</p>
+
+			<h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+			<p>
+				Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+				Verbraucherschlichtungsstelle teilzunehmen.
+			</p>
+
+			<p>
+				Quelle:{' '}
+				<a href='https://www.e-recht24.de/impressum-generator.html'>
+					https://www.e-recht24.de/impressum-generator.html
+				</a>
+			</p>
+		</section>
+	);
+}
+
+export default Impressum;
