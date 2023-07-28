237eb55ab0084bd5b3f53ac0943c799c4bab1a94
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-07-28 18:50:19 +0200

    Make logo clickable to go back to /, add class for unactive Links, add :hover effect to Links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,40 +7,47 @@ import styles from './Navbar.module.scss';
 function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<img src={Logo} alt='Fliesenfachbetrieb Müller Logo' />
+			<NavLink to='/'>
+				<img src={Logo} alt='Fliesenfachbetrieb Müller Logo' />
+			</NavLink>
 			<ul>
 				<li>
 					<NavLink
 						to='/'
-						className={({ isActive }) => (isActive ? styles.active : undefined)}>
+						className={({ isActive }) => (isActive ? styles.activeLink : styles.unactiveLink)}
+						data-content='Home'>
 						Home
 					</NavLink>
 				</li>
 				<li>
 					<NavLink
 						to='/about'
-						className={({ isActive }) => (isActive ? styles.active : undefined)}>
+						className={({ isActive }) => (isActive ? styles.activeLink : styles.unactiveLink)}
+						data-content='Warum Uns'>
 						Warum Uns
 					</NavLink>
 				</li>
 				<li>
 					<NavLink
 						to='/inspirationen'
-						className={({ isActive }) => (isActive ? styles.active : undefined)}>
+						className={({ isActive }) => (isActive ? styles.activeLink : styles.unactiveLink)}
+						data-content='Inspirationen'>
 						Inspirationen
 					</NavLink>
 				</li>
 				<li>
 					<NavLink
 						to='/bewertungen'
-						className={({ isActive }) => (isActive ? styles.active : undefined)}>
+						className={({ isActive }) => (isActive ? styles.activeLink : styles.unactiveLink)}
+						data-content='Bewertungen'>
 						Bewertungen
 					</NavLink>
 				</li>
 				<li>
 					<NavLink
 						to='/kontakt'
-						className={({ isActive }) => (isActive ? styles.active : undefined)}>
+						className={({ isActive }) => (isActive ? styles.activeLink : styles.unactiveLink)}
+						data-content='Kontakt'>
 						Kontakt
 					</NavLink>
 				</li>
