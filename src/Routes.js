56e7a8a76ba3f45682f40238d700c5a4db283c04
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-08-01 20:56:08 +0200

    add starter files and code for transition group

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 import App from './components/App';
 import Home from './pages/Home';
 import Impressum from './pages/Impressum';
 import Datenschutz from './pages/Datenschutz';
 
+import styles from './Routes.module.scss';
+
 // Routing displays by default Home component
 function Routing() {
+	const location = useLocation();
+
 	return (
-		<Routes>
-			<Route path='/' element={<App />}>
-				<Route index element={<Home />} />
-				<Route path='about' element={<Home scrollTo={'aboutSection'} />} />
-				<Route path='inspirationen' element={<Home scrollTo={'inspoSection'} />} />
-				<Route path='neukunde' element={<Home scrollTo={'couponSection'} />} />
-				<Route path='bewertungen' element={<Home scrollTo={'ratingSection'} />} />
-				<Route path='kontakt' element={<Home scrollTo={'contactSection'} />} />
-				<Route path='impressum' element={<Impressum />} />
-				<Route path='datenschutz' element={<Datenschutz/>} />
-			</Route>
-		</Routes>
+		<TransitionGroup>
+			<CSSTransition
+				key={location.key}
+				timeout={500}
+				classNames={{
+					enterActive: styles.pageEnterActive,
+					enterDone: styles.pageEnterDone,
+					exitActive: styles.pageExit,
+					exitDone: styles.pageExitActive,
+				}}>
+				<Routes location={location}>
+					<Route path='/' element={<App />}>
+						<Route index element={<Home />} />
+						<Route path='about' element={<Home scrollTo={'aboutSection'} />} />
+						<Route path='inspirationen' element={<Home scrollTo={'inspoSection'} />} />
+						<Route path='neukunde' element={<Home scrollTo={'couponSection'} />} />
+						<Route path='bewertungen' element={<Home scrollTo={'ratingSection'} />} />
+						<Route path='kontakt' element={<Home scrollTo={'contactSection'} />} />
+						<Route path='impressum' element={<Impressum />} />
+						<Route path='datenschutz' element={<Datenschutz />} />
+					</Route>
+				</Routes>
+			</CSSTransition>
+		</TransitionGroup>
 	);
 }
 
