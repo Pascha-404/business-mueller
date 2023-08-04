1d0bf4d1f65213c3389bec7f7844feefa3436923
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-08-04 22:46:53 +0200

    undo PageTransition wrap of Outlet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,9 @@ import { Outlet } from 'react-router-dom';
 import { BtnThemeProvider } from '../../contexts/btnTheme.context';
 import { ContactProvider } from '../../contexts/contact.context';
 
-
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import CookieBanner from '../CookieBanner/CookieBanner';
-import PageTransition from '../PageTransition'
 
 function App() {
 	return (
@@ -17,9 +15,7 @@ function App() {
 			<main>
 				<CookieBanner />
 				<BtnThemeProvider>
-					<PageTransition>
-						<Outlet />
-					</PageTransition>
+					<Outlet />
 				</BtnThemeProvider>
 			</main>
 			<Footer />
