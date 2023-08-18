9849d8171c061c2f9b2b27827fbd3c33fa33c8c2
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-08-18 19:48:25 +0200

    refactor useEffect to use state of useLocation hook instead of scrollTo provided through route

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import HeroSection from '../../components/HeroSection';
 import AboutSection from '../../components/AboutSection';
 import InspoSection from '../../components/InspoSection';
@@ -8,18 +10,21 @@ import ContactSection from '../../components/ContactSection';
 import CouponSection from '../../components/CouponSection';
 
 // Gathers all Sections to display Homeview in <main> element
-function Home({ scrollTo }) {
+function Home() {
+	const location = useLocation();
+
+	// If location.state has a value, scrolls to provided section
 	useEffect(() => {
 		let targetSection;
-		if (scrollTo) {
-			targetSection = document.querySelector(`#${scrollTo}`);
+		if (location.state !== null) {
+			targetSection = document.querySelector(`#${location.state.section}`);
 			targetSection.scrollIntoView();
 		}
 
 		return () => {
 			targetSection = null;
 		};
-	}, [scrollTo]);
+	}, [location]);
 
 	return (
 		<Fragment>
