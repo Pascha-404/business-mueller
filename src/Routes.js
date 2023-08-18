62c4a49721f0b1166f3186717b1e1af7137fa8ea
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-08-18 19:35:00 +0200

    add comment, delete name key in objects of routes array

--- src/Routes.js
+++ src/Routes.js
@@ -6,30 +6,13 @@ import Home from './pages/Home';
 import Impressum from './pages/Impressum';
 import Datenschutz from './pages/Datenschutz';
 
-// Routing displays by default Home component
-// function Routing() {
-// 	return (
-// 		<Routes>
-// 			<Route path='/' element={<App />}>
-// 				<Route index element={<Home />} />
-// 				<Route path='about' element={<Home scrollTo={'aboutSection'} />} />
-// 				<Route path='inspirationen' element={<Home scrollTo={'inspoSection'} />} />
-// 				<Route path='neukunde' element={<Home scrollTo={'couponSection'} />} />
-// 				<Route path='bewertungen' element={<Home scrollTo={'ratingSection'} />} />
-// 				<Route path='kontakt' element={<Home scrollTo={'contactSection'} />} />
-// 				<Route path='impressum' element={<Impressum />} />
-// 				<Route path='datenschutz' element={<Datenschutz />} />
-// 			</Route>
-// 		</Routes>
-// 	);
-// }
-
+// Array of route objects for the different pages of the application
 const routes = [
-	{ path: '/', name: 'Home', element: <Home />, nodeRef: createRef() },
-	{ path: '/impressum', name: 'Impressum', element: <Impressum />, nodeRef: createRef() },
+	{ path: '/', element: <Home />, nodeRef: createRef() },
+	{ path: '/impressum', element: <Impressum />, nodeRef: createRef() },
 	{
 		path: '/datenschutz',
-		name: 'Datenschutz',
+
 		element: <Datenschutz />,
 		nodeRef: createRef(),
 	},
