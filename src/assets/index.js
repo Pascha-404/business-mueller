a85dc8b62da56029923b3ce73ccccd35ceb34ad6
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-08-20 21:02:28 +0200

    add object filled with keys to images on cloudinary

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,6 +20,52 @@ import iconStar from './icon-star.png';
 import iconSuccess from './icon-success.png';
 import iconError from './icon-error.png';
 
+const cloudinaryUrls = {
+	textureOne:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543311/texture-1_pllejd.png',
+
+	textureTwo:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543313/texture-2_zxw8cn.png',
+	iconCompass:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543307/icon-compass_mvffrs.png',
+	iconPhone:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543308/icon-phone_yfkp0n.png',
+	iconTile:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543308/icon-tile_s1flm7.png',
+	iconQuestionMark:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543308/icon-questionMark_jvzvhd.png',
+	iconClipboard:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543307/icon-clipboard_beh1gi.png',
+	iconHouse:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543307/icon-house_eowlfw.png',
+	iconDiamond:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543307/icon-diamond_ysfe3s.png',
+	iconLightbulb:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543308/icon-lightbulb_h5vf9w.png',
+	imgAbout:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543309/img-about_gv57e4.png',
+	imgHero:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543311/img-hero_zcqkmg.png',
+	inspoOne:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543310/inspo-1_zksxdq.png',
+	inspoThree:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543310/inspo-3_jnise3.png',
+	logoMueller:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543308/logo-mueller_n0qxhe.png',
+	bgService:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543313/service-bg_br4mr9.png',
+	iconContact:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543307/icon-email_q68lxt.png',
+	imgContact:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543313/img-contact_rtrsjs.png',
+	iconStar:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543308/icon-star_ebuglt.png',
+	iconSuccess:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543308/icon-success_yzmutu.png',
+	iconError:
+		'https://res.cloudinary.com/drpmdiapv/image/upload/v1692543307/icon-error_mpjw6s.png',
+};
+
 export {
 	iconCompass,
 	iconPhone,
@@ -42,4 +88,5 @@ export {
 	iconStar,
 	iconSuccess,
 	iconError,
+	cloudinaryUrls
 };
