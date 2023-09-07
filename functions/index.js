b4185f715aebde15951523dd05330b04e0ff4857
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-09-07 23:28:08 +0200

    fix function import, destructure snap.data in function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
+const constructCustomerMail = require('./constructCustomerMail');
 
 admin.initializeApp();
 
@@ -53,30 +54,13 @@ exports.sendEmail = functions.firestore
 exports.sendEmailToCustomer = functions.firestore
 	.document('mails/{mailId}')
 	.onCreate((snap, context) => {
-		const callBack = snap.data().phoneCall
-			? `<p>Telefonnummer: ${
-					snap.data().phoneNumber
-			  }</p><p>Sie wünschen einen Rückruf</p>`
-			: '<p>Sie wünschen die Kontaktaufnahme via Email</p>';
+		const { name, email, message, phoneCall, phoneNumber } = snap.data();
 
 		const mailOptions = {
 			from: process.env.MAIL_USER,
-			to: snap.data().email,
+			to: email,
 			subject: `Ihre Anfrage ist eingegangen`,
-			html: `<h1>Kontaktformular eingegangen</h1>
-					<p>Sehr geehrte Damen und Herren, <br> herzlichen Dank für ihre Kontaktanfrage. Wir werden uns so schnell wie möglich mit Ihnen in Verbindungen setzen.</p>
-					<p>Unten finden sie noch mal die von Ihnen angegebenen Daten.</p>
-                    <br>
-					<p>Dies ist eine automatisch generierte Eingangsbestätigung. Bitte antworten Sie nicht auf diese E-Mail.</p>
-					<br>
-					<p>Freundlich grüßt Sie Ihr</p>
-					<p>Fliesenfachbetrieb Müller</p>
-					<br>
-                    <p>Name: ${snap.data().name}</p>
-                    <p>Email: ${snap.data().email}</p>
-                    ${callBack}
-                    <p>Nachricht: ${snap.data().message}</p>
-					`,
+			html: constructCustomerMail({ name, email, message, phoneCall, phoneNumber }),
 		};
 
 		return transporter.sendMail(mailOptions, (error, data) => {
