bcab4db083de7ee2426e6749ffaa92dc1ad657ac
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-09-12 21:41:17 +0200

    modify sendEmail function to use constructBusinessMail function for html construction

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
 const constructCustomerMail = require('./constructCustomerMail');
+const constructBusinessMail = require('./constructBusinessMail');
 
 admin.initializeApp();
 
@@ -21,23 +22,14 @@ const transporter = nodemailer.createTransport({
 exports.sendEmail = functions.firestore
 	.document('mails/{mailId}')
 	.onCreate((snap, context) => {
-		const callBack = snap.data().phoneCall
-			? `<p>Telefonnummer: ${
-					snap.data().phoneNumber
-			  }</p><p>Der Kunde wünscht einen Rückruf</p>`
-			: '<p>Der Kunde wünscht die Kontaktaufnahme via Email</p>';
+		const { name, email, message, phoneCall, phoneNumber } = snap.data();
 
 		const mailOptions = {
 			from: process.env.MAIL_USER,
 			to: process.env.MAIL_RECIEVER,
-			replyTo: snap.data().email,
-			subject: `Anfrage von ${snap.data().name}`,
-			html: `<h1>Kontaktformular eingegangen</h1>
-                    <br>
-                    <p>Name: ${snap.data().name}</p>
-                    <p>Email: ${snap.data().email}</p>
-                    ${callBack}
-                    <p>Nachricht: ${snap.data().message}</p>`,
+			replyTo: email,
+			subject: `Anfrage von ${name}`,
+			html: constructBusinessMail({ name, email, message, phoneCall, phoneNumber }),
 		};
 
 		return transporter.sendMail(mailOptions, (error, data) => {
